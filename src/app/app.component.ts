525e713e42cd500cd33e08b3d526bc6f9e7e5548
johnasblasco <johnaslblasco@gmail.com>
2024-06-03 20:13:20 +0800

    updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
